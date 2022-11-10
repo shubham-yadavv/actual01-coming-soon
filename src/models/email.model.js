efc1f8e2e64f94984708f5cbd19fb8e935f11db4
shubham <shubham22121@gmail.com>
2022-11-10 11:35:39 +0530

    email date

--- src/models/email.model.js
+++ src/models/email.model.js
@@ -5,6 +5,10 @@ const emailSchema = new mongoose.Schema({
     type: String,
     required: true
   },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
 });
 
 const Email = mongoose.model("Email", emailSchema);
