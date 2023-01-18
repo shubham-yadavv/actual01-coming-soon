5b3e8a6e408ffdb3ed5d03e379af63380dbc8390
shubham <shubham22121@gmail.com>
2023-01-19 00:08:15 +0530

    date and time for emails

--- src/models/email.model.js
+++ src/models/email.model.js
@@ -1,10 +1,15 @@
 const mongoose = require("mongoose");
+const moment = require("moment");
 
 const emailSchema = new mongoose.Schema({
   email: {
     type: String,
     required: true
   },
+  createdAt : {
+    type: String,
+    default: moment().format("MMMM Do YYYY, h:mm:ss a")
+  }
 });
 
 const Email = mongoose.model("Email", emailSchema);
