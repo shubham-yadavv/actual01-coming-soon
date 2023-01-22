b5c2758554f505b8e94500f734da639767f8a6df
shubham <shubham22121@gmail.com>
2023-01-22 16:42:49 +0530

    event email

--- src/controllers/email.controller.js
+++ src/controllers/email.controller.js
@@ -1,6 +1,8 @@
 const Email = require("../models/email.model");
 const sendEmail = require("../utils/sendEmail.utils");
 const fs = require("fs");
+const util = require('util');
+
 
 const saveEmail = async (req, res) => {
   let readFile = fs.readFileSync("./src/utils/email/early-access.html", "utf8");
@@ -13,7 +15,6 @@ const saveEmail = async (req, res) => {
         email: oldEmail.email,
         subject: "Your registration has been confirmed.",
         html: readFile,
-        // message: "You are already subscribed to my website",
       });
     } else {
       await email.save();
@@ -30,6 +31,18 @@ const saveEmail = async (req, res) => {
     res.status(400).json({ error: error.message });
   }
 };
+
+// saveEmail with no email
+const saveEmailOne = async (req, res) => {
+  try {
+    const email = new Email(req.body);
+    await email.save();
+    res.status(201).json({ email });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
 
 // get all emails
 const getEmails = async (req, res) => {
@@ -55,8 +68,33 @@ const deleteEmail = async (req, res) => {
   }
 };
 
+// send a email to all emails in the database one by one with a delay of 5 seconds
+const sendEmailInBulk = async (req, res) => {
+  let eventHtml = fs.readFileSync("./src/utils/email/event.html", "utf8");
+  try {
+    const emails = await Email.find();
+
+    for (let i = 0; i < emails.length; i++) {
+      await sendEmail({
+        email: emails[i].email,
+        subject: "Your registration has been approved",
+        html : eventHtml,
+      });
+      await util.promisify(setTimeout)(5000);
+    }
+    res.status(200).json({ message: "Emails sent" });
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ error: error.message });
+  }
+};
+
+
+
 module.exports = {
   saveEmail,
   getEmails,
   deleteEmail,
+  sendEmailInBulk,
+  saveEmailOne
 };
--- src/routes/email.routes.js
+++ src/routes/email.routes.js
@@ -5,12 +5,22 @@ const {
   saveEmail,
   getEmails,
   deleteEmail,
+  sendEmailInBulk,
+  saveEmailOne
 } = require("../controllers/email.controller");
 
 const router = express.Router();
 
 router.route("/emails").get(getEmails);
 router.route("/emails").post( saveEmail);
+
+router.route("/emails/bulk").post(sendEmailInBulk); 
+
+router.route("/emails/one").post(saveEmailOne);
+
+
+
+
 router
   .route("/emails/:id")
   .delete(deleteEmail);
